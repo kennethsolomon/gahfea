ac8ceb3a1eb5d53fc28f7a68882642eaf67267aa
Kenneth Solomon <synecders@gmail.com>
2020-01-02 13:26:23 +0800

    Fix the insert query for Registration

--- pages/adduser.php
+++ pages/adduser.php
@@ -2,13 +2,10 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                     <h4 class="modal-title" id="myModalLabel">Add User</h4>
                 </div>
                 <div class="modal-body">
-
-
                     <form action="saveuser.php" method="post" class = "form-group" >
                         <div id="ac">
                             <span>USERNAME : </span><input type="text" name="user" class = "form-control" />
--- pages/saveuser.php
+++ pages/saveuser.php
@@ -5,13 +5,9 @@ $a = $_POST['user'];
 $b = $_POST['pass'];
 $c = $_POST['name'];
 $d = $_POST['post'];
-if($d == 'Cashier'){
-    $sql = "INSERT INTO cashier (username,password,cashier_name,position) VALUES (?,?,?,?)";
-} else if($d == 'Kitchen'){
-    $sql = "INSERT INTO kitchen (username,password,kitchen_name,position) VALUES (?,?,?,?)";
-} else if($d == 'Front Attendant'){
-    $sql = "INSERT INTO attendant (username,password,attendant_name,position) VALUES (?,?,?,?)";
-} 
+
+$sql = "INSERT INTO user (username,password,name,position) VALUES (?,?,?,?)";
+
 $q = $db->prepare($sql);
 $q->execute(array($a,$b,$c,$d));
 header("location: home.php");
